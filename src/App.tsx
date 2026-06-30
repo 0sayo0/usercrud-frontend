2d67981b58364e76f824d78da1a45493106d5676
Jonathan Morales <jonathansme01@gmail.com>
2026-06-30 14:25:16 -0600

    Prepare environment for development

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1>Hello world!</h1>
+      <h1 className="font-black text-2xl text-teal-300 bg-pink-500 rounded-lg m-10 p-2">
+        Hello world!
+      </h1>
     </>
   );
 }
